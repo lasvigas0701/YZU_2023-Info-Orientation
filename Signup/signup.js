eaa9462a9193a63634068e3b2be2cd879a98e766
lasvigas0701 <weichia0701@gmail.com>
2025-03-07 12:24:19 +0800

    remove something

--- Signup/signup.js
+++ Signup/signup.js
@@ -3,13 +3,13 @@ import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.1.0
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD6htxCy7CxDOaP_woenezkEKoOX3o6d1E",
+  apiKey: "",
   authDomain: "ic-im-in-cse-2023.firebaseapp.com",
   databaseURL: "https://ic-im-in-cse-2023-default-rtdb.firebaseio.com",
   projectId: "ic-im-in-cse-2023",
   storageBucket: "ic-im-in-cse-2023.appspot.com",
   messagingSenderId: "589513037614",
-  appId: "1:589513037614:web:d38e85f91294bf6819ccbf"
+  appId: ""
 };
 
 
